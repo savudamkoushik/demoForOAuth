434af11d5d89061150b47aa662f19bd7237f89aa
koushiksavudam <savudamkoushik@gmail.com>
2020-08-22 15:03:29 +0530

    Initial commit

---
+++ src/main/java/com/example/demoForOAuth/DemoForOAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoForOAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoForOAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoForOAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoForOAuth/DemoForOAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoForOAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoForOAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
